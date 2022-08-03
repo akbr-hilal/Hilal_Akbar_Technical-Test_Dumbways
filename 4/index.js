c87e734d5945119554b66333a8974c9ad2ffacbf
akbr-hilal <dev.akbrhilal@gmail.com>
2022-08-03 23:56:21 +0700

    update

--- 4/index.js
+++ 4/index.js
@@ -23,11 +23,11 @@ db.connect((err) => {
   console.log("Database connected.....")
 
   app.get("/", (req, res) => {
-    const selectProvinsi = `SELECT * FROM provinsi_tb`
+    const selectProvinsi = `SELECT * FROM provinsi_tb UNION SELECT * FROM kabupaten_tb`
     
     db.query(selectProvinsi, (err, result) => {
       if(err) throw err
-      console.log(result)
+      // console.log(result)
       const data = JSON.parse(JSON.stringify(result))
       console.log(data)
 
@@ -35,6 +35,7 @@ db.connect((err) => {
         data: data
       })
     })
+
   })
 
   app.get("/add-provinsi", (req, res) => {
@@ -53,10 +54,12 @@ db.connect((err) => {
     })
   })
 
-  app.get("/detail-provinsi/:id", (req, res) => {
+  app.get("/detail/:id", (req, res) => {
     let id = req.params.id
 
-    db.query(`SELECT * FROM provinsi_tb WHERE id=${id}`, (err, result) => {
+    const selectProvinsi = `SELECT * FROM provinsi_tb WHERE id=${id} UNION SELECT * FROM kabupaten_tb WHERE id=${id}`
+
+    db.query(selectProvinsi, (err, result) => {
       if(err) throw err
 
       console.log(result)
@@ -67,7 +70,7 @@ db.connect((err) => {
 
       console.log(data)
 
-      res.render("detail-provinsi", {
+      res.render("detail", {
         data: data
       })
     })
@@ -82,11 +85,67 @@ db.connect((err) => {
       res.redirect("/")
     })
   })
+
+  app.get("/edit/:id", (req, res) => {
+    db.query(`SELECT * FROM provinsi_tb WHERE id =${req.params.id}`, (err, result) => {
+      if(err) throw err
+
+      let data = result[0]
+
+      data = JSON.parse(JSON.stringify(data))
+
+      console.log(data)
+
+      res.render("edit-provinsi", {
+        data: data
+      })
+    })
+  })
+
+  app.post("/edit/:id", upload.single("inputImageProv"), (req, res) => {
+    let id = req.params.id
+    let data = req.body
+    let img = req.file.filename
+
+    let updateData = `UPDATE provinsi_tb SET name='${data.inputNameProv}', diresmikan='${data.inputDiresmikanProv}', pulau='${data.inputPulauProv}', img='${img}' WHERE id=${id}`
+
+    db.query(updateData, (err, result) => {
+      if(err) throw err
+      res.redirect("/")
+    })
+  })
+
+  app.get("/add-kabkot", upload.single("inputImageKabKot"), (req, res) => {
+    let provinsi = "SELECT name, id FROM provinsi_tb WHERE 1"
+
+    db.query(provinsi, (err, result) => {
+      if (err) throw err
+
+      // console.log(result)
+
+      const data = JSON.parse(JSON.stringify(result))
+
+      console.log(data)
+
+      res.render("add-kabkota", {
+        data: data
+      })
+    })
+  })
+
+  app.post("/add-kabkot", upload.single("inputImageKabKot"), (req, res) => {
+    let data = req.body;
+    let img = req.file.filename;
+
+    let insertKabKot = `INSERT INTO kabupaten_tb(name, diresmikan, img, provinsi_id) VALUES ('${data.inputNameKabKot}','${data.inputDiresmikanKabKot}','${img}','${data.inputProvinsiId}')`
+
+    db.query(insertKabKot, (err, result) => {
+      if(err) throw err
+      console.log(result)
+      res.redirect("/")
+    })
+  })
 })
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Server on ${port}`)
